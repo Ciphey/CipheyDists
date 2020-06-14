ba466394cc7c23479f37878d4cd18281b138acdc
Cyclic3 <cyclic3.git@gmail.com>
2020-06-14 19:29:54 +0100

    Send the model data instead of the path

--- cipheydists/__init__.py
+++ cipheydists/__init__.py
@@ -20,6 +20,4 @@ def get_list(name: str):
 
 def get_model(name: str):
   if check_name(name):
-    # The code for this was taken from the pkgutil docs
-    d = os.path.dirname(sys.modules[__name__].__file__)
-    return os.path.join(d, "model", name + ".tflite")
+    return pkgutil.get_data(__name__, os.path.join("model", name) + ".tflite")
